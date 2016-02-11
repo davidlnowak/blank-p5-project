0c4121eeb738b87fed8d8c1355e7764d02305157
davidlnowak <nowak.david@gmail.com>
2016-02-11 09:13:28 -0500

    Initial Coding
    Pop 500 bubbles to the screen.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
-var setup = function setup() {
+var bubbles = [];
 
-}
+var setup = function() {
+  createCanvas(800, 600);
 
-var draw = function () {
+  loadBubbles(500);
+};
 
-}
+var loadBubbles = function(totalBubbles) {
+  for (var index = 0; index < totalBubbles; ++index) {
+    bubbles.push(new Bubble(random(25, width-25), random(100, height)))
+  }
+};
+
+var draw = function() {
+  background(100);
+
+  bubbles.forEach(function updateAndDisplay (bubble) {
+    bubble.display();
+    bubble.update();
+  });
+};
