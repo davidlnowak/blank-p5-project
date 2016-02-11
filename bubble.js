6ef3b741eaa8cc356da286fdc8dfdc98756146b8
davidlnowak <nowak.david@gmail.com>
2016-02-11 09:51:08 -0500

    Initial Coding
    Same coding as in Bubbles3, but the forEach loop has been changed to
    While.

---
+++ bubble.js
@@ -0,0 +1,22 @@
+var Bubble = function (x, y) {
+    this.x = x;
+    this.y = y;
+};
+
+Bubble.prototype = {
+
+    radius: 5,
+
+    update: function() {
+        this.x += random(-1, 1);
+        this.y += random(0, -2);
+        if (this.y - this.radius <= 0) this.y = height-this.radius;
+    },
+
+    display: function () {
+        noStroke();
+        fill(190, 32, 32, 100);
+        ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    }
+
+};
--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
-var setup = function setup() {
+var bubbles = [];
 
-}
+var loadBubbles = function(totalBubbles) {
+  for (var index = 0; index < totalBubbles; ++index) {
+    bubbles.push(new Bubble(random(25, width-25), random(height/2-100, height/2+100)))
+  }
+};
 
-var draw = function () {
+var setup = function() {
+  createCanvas(800, 600);
 
-}
+  loadBubbles(500);
+};
+
+var draw = function() {
+  background(100);
+
+  var currentBubble = 0
+  while (currentBubble < 500) {
+    bubbles[currentBubble].display();
+    bubbles[currentBubble].update();
+
+    currentBubble++;
+  };
+};
