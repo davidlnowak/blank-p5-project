d63a9dacae281ccafc010ad266dc042d88e186f2
davidlnowak <nowak.david@gmail.com>
2016-02-11 08:55:15 -0500

    Initial Coding
    Draw bubbles when mouse is clicked.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
-var setup = function setup() {
+var bubbles = [];
 
-}
+var setup = function() {
+  createCanvas(800, 600);
+};
 
-var draw = function () {
+var draw = function() {
+  background(100);
 
-}
+  if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY));
+
+  bubbles.forEach(function updateAndDisplay (bubble) {
+    bubble.display();
+    bubble.update();
+  });
+};
