c9299786c68cd2cdd9863d06b4cda61a048dec86
davidlnowak <nowak.david@gmail.com>
2016-03-22 19:19:43 -0400

    Complete Working Application
    All coding complete for this application.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
-var setup = function () {
-  // your code goes here
-};
+var setup = function setup() {
+  createCanvas(600, 600);
+  background(100);
 
-var draw = function () {
-  // your code goes here
+  // Rocket nose
+  fill('#DF3A01');
+  triangle(300, 50, 240, 150, 360, 150);
+
+  // Rocket body
+  fill('#D8D8D8');
+  rect(240, 150, 120, 350);
+
+  // Window
+  fill('#030A6B');
+  ellipse(300, 225, 60, 60);
+
+  fill('#81BEF7');
+  ellipse(300, 225, 48, 48);
+
+  // Fins
+  fill('#DF3A01');
+  quad(240, 400, 175, 460, 175, 500, 240, 475);
+  quad(360, 400, 425, 460, 425, 500, 360, 475);
+
+  // Thruster
+  fill('#2E2E2E');
+  quad(250, 500, 245, 517, 355, 517, 350, 500);
 };
