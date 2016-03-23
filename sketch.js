760c05d8f593e81616a2fa977709a189b7051775
davidlnowak <nowak.david@gmail.com>
2016-03-23 13:20:45 -0400

    Complete Working Application
    All coding complete for this application.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
 var setup = function () {
-  // your code goes here
-};
+  createCanvas(600, 500);
+  background(80);
 
-var draw = function () {
-  // your code goes here
+  // Left Wing
+  fill(255);
+  triangle(80,210, 500,100, 190,255);
+
+  // Left Fold
+  fill(200);
+  triangle(190,255, 500,100, 210,310);
+
+  // Right Wing
+  fill(255);
+  triangle(245,270, 500,100, 360,340);
+
+  // Right Fold
+  fill(140);
+  triangle(245,270, 210,310, 270,285);
+
+  // Trail Lines
+  strokeWeight(5);
+  line(200,330, 90,420);
+  line(280,310, 187,400);
 };
